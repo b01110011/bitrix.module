abd6a0fe65c7ee429602c38104313fe769f3220f
b01110011 <b01110011b@gmail.com>
2020-01-12 00:25:58 +0700

    В файле можно использовать только двойные кавычки, одинарные работать не будут.

--- bitrix.module/install/version.php
+++ bitrix.module/install/version.php
@@ -1,7 +1,8 @@
 <?php
 
+// должны быть " кавычки
 $arModuleVersion =
 [
-    'VERSION' => '1.0.0',
-    'VERSION_DATE' => '2020.01.01 20:47:00'
+    "VERSION" => "1.0.0",
+    "VERSION_DATE" => "2020.01.01 20:47:00"
 ];
